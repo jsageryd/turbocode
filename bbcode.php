07416680f8bbaddd6f33726fb25e8705dd6048bf
Johan Sageryd <j416@1616.se>
2010-05-23 19:35:26 +0200

    Add comments

--- bbcode.php
+++ bbcode.php
@@ -13,36 +13,36 @@ function bbcode($string)
 #  $string = preg_replace("#\[img-l\](.+?)\[/img\]#is", "<img src=\"\\1\" alt=\"[image]\" style=\"border: thin solid #DFE5F2; FLOAT: left; MARGIN-RIGHT: 20px\" />", $string);
 #  $string = preg_replace("#\[img-r\](.+?)\[/img\]#is", "<img src=\"\\1\" alt=\"[image]\" style=\"border: thin solid #DFE5F2; FLOAT: right; MARGIN-LEFT: 20px;\" />", $string);
 
-# Spans
+# Spans [s:theclass]Text[/s] -or- \theclass[Text]
   $string = preg_replace("#\[s:(.+)\](.+?)\[/s\]#is", "<span class=\"$1\">$2</span>", $string);
   $string = preg_replace("#\\(.+)\[(.+?)\]#is", "<span class=\"$1\">$2</span>", $string);
 
-# Divs
+# Divs [d:theclass]Text[/d] -or- \theclass{Text}
   $string = preg_replace("#\[d:(.+)\](.+?)\[/d\]#is", "<div class=\"$1\">$2</div>", $string);
   $string = preg_replace("#\\(.+)\{(.+?)\}#is", "<div class=\"$1\">$2</div>", $string);
 
-# Paragraphs
+# Paragraphs [paragraph:The Paragraph]
   do{
     $laststring = $string;
     $string = preg_replace("#\[paragraph:(.+)?\](.*)?\[(paragraph|section|subsection|subsubsection):(.+)?\]#isU", "<div class=\"paragraph\"><h4>$1</h4>$2</div>[$3:$4]", $string);
   }while($string !== $laststring);
   $string = preg_replace("#\[paragraph:(.+)?\](.*)$#isU", "<div class=\"paragraph\"><h4>$1</h4>$2</div>", $string);
 
-# Subsubsections
+# Subsubsections [subsubsection:The Subsubsection]
   do{
     $laststring = $string;
     $string = preg_replace("#\[subsubsection:(.+)?\](.*)?\[(sub|subsub)?section:(.+)?\]#isU", "<div class=\"subsubsection\"><h3>$1</h3>$2</div>[$3section:$4]", $string);
   }while($string !== $laststring);
   $string = preg_replace("#\[subsubsection:(.+)?\](.*)$#isU", "<div class=\"subsubsection\"><h3>$1</h3>$2</div>", $string);
 
-# Subsections
+# Subsections [subsection:The Subsection]
   do{
     $laststring = $string;
     $string = preg_replace("#\[subsection:(.+)?\](.*)?\[(sub)?section:(.+)?\]#isU", "<div class=\"subsection\"><h2>$1</h2>$2</div>[$3section:$4]", $string);
   }while($string !== $laststring);
   $string = preg_replace("#\[subsection:(.+)?\](.*)$#isU", "<div class=\"subsection\"><h2>$1</h2>$2</div>", $string);
 
-# Sections
+# Sections [section: The Section]
   do{
     $laststring = $string;
     $string = preg_replace("#\[section:(.+)?\](.*)?\[section:(.+)?\]#isU", "<div class=\"section\"><h1>$1</h1>$2</div>[section:$3]", $string);
