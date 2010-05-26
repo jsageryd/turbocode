b1a6ea33d04e56fcd800cf1bb1535a1536e28afd
Johan Sageryd <j416@1616.se>
2010-05-26 14:59:09 +0200

    Revert "Move possible threats to stability last"
    This reverts commit 051e0288b2fea3aafdcf5c5723747bf75136b72a.

--- turbocode.php
+++ turbocode.php
@@ -1,4 +1,17 @@
 function turbocode($string){
+#	$string = preg_replace("#\[b\](.+?)\[/b\]#is", "<b>\\1</b>", $string);
+#	$string = preg_replace("#\[i\](.+?)\[/i\]#is", "<i>\\1</i>", $string);
+#	$string = preg_replace("#\[u\](.+?)\[/u\]#is", "<u>\\1</u>", $string);
+#	$string = preg_replace("#\[link\]www\.(.+?)\[/link\]#is", "<a href=\"http://www.\\1\">www.\\1</a>", $string);
+#	$string = preg_replace("#\[link\](.+?)\[/link\]#is", "<a href=\"\\1\">\\1</a>", $string);
+#	$string = preg_replace("#\[link=(.+?)\](.+?)\[/link\]#is", "<a href=\"\\1\">\\2</a>", $string);
+#	$string = preg_replace("#\[url\]www\.(.+?)\[/url\]#is", "<a href=\"http://www.\\1\">www.\\1</a>", $string);
+	$string = preg_replace("#\[url\](.+?)\[/url\]#is", "<a href=\"$1\">$1</a>", $string);
+	$string = preg_replace("#\[url=(.+?)\](.+?)\[/url\]#is", "<a href=\"$1\">$2</a>", $string);
+#	$string = preg_replace("#\[img\](.+?)\[/img\]#is", "<img src=\"\\1\" alt=\"[image]\" style=\"margin: 5px 0px 5px 0px\" />", $string);
+#	$string = preg_replace("#\[img-l\](.+?)\[/img\]#is", "<img src=\"\\1\" alt=\"[image]\" style=\"border: thin solid #DFE5F2; FLOAT: left; MARGIN-RIGHT: 20px\" />", $string);
+#	$string = preg_replace("#\[img-r\](.+?)\[/img\]#is", "<img src=\"\\1\" alt=\"[image]\" style=\"border: thin solid #DFE5F2; FLOAT: right; MARGIN-LEFT: 20px;\" />", $string);
+
 	do{
 		$laststring = $string;
 
@@ -43,19 +56,6 @@ function turbocode($string){
 		$string = preg_replace("#\[section:(.+)\](.*)\[section:(.+)\]#isU", "<div class=\"tc_section\"><h1>$1</h1>$2</div>\n[section:$3]", $string, 1);
 	}while($string !== $laststring);
 	$string = preg_replace("#\[section:(.+)\](.*)$#isU", "<div class=\"tc_section\"><h1>$1</h1>$2</div>", $string);
-
-#	$string = preg_replace("#\[b\](.+?)\[/b\]#is", "<b>\\1</b>", $string);
-#	$string = preg_replace("#\[i\](.+?)\[/i\]#is", "<i>\\1</i>", $string);
-#	$string = preg_replace("#\[u\](.+?)\[/u\]#is", "<u>\\1</u>", $string);
-#	$string = preg_replace("#\[link\]www\.(.+?)\[/link\]#is", "<a href=\"http://www.\\1\">www.\\1</a>", $string);
-#	$string = preg_replace("#\[link\](.+?)\[/link\]#is", "<a href=\"\\1\">\\1</a>", $string);
-#	$string = preg_replace("#\[link=(.+?)\](.+?)\[/link\]#is", "<a href=\"\\1\">\\2</a>", $string);
-#	$string = preg_replace("#\[url\]www\.(.+?)\[/url\]#is", "<a href=\"http://www.\\1\">www.\\1</a>", $string);
-	$string = preg_replace("#\[url\](.+?)\[/url\]#is", "<a href=\"$1\">$1</a>", $string);
-	$string = preg_replace("#\[url=(.+?)\](.+?)\[/url\]#is", "<a href=\"$1\">$2</a>", $string);
-#	$string = preg_replace("#\[img\](.+?)\[/img\]#is", "<img src=\"\\1\" alt=\"[image]\" style=\"margin: 5px 0px 5px 0px\" />", $string);
-#	$string = preg_replace("#\[img-l\](.+?)\[/img\]#is", "<img src=\"\\1\" alt=\"[image]\" style=\"border: thin solid #DFE5F2; FLOAT: left; MARGIN-RIGHT: 20px\" />", $string);
-#	$string = preg_replace("#\[img-r\](.+?)\[/img\]#is", "<img src=\"\\1\" alt=\"[image]\" style=\"border: thin solid #DFE5F2; FLOAT: right; MARGIN-LEFT: 20px;\" />", $string);
 
 	return $string;
 }
